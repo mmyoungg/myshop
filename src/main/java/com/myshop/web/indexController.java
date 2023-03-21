ae8259d45ffbb6a6c54131d343643430c3d0fe2e
mmyoungg <die199074@gmail.com>
2023-03-21 18:47:28 +0900

    [feat] Spring Security+OAuth2.0 구글 소셜로그인 구현

--- src/main/java/com/myshop/web/indexController.java
+++ src/main/java/com/myshop/web/indexController.java
@@ -1,23 +1,31 @@
 package com.myshop.web;
 
+import com.myshop.config.auth.dto.SessionUser;
 import com.myshop.service.posts.PostsService;
-import com.myshop.web.dto.PostsListResponseDto;
 import com.myshop.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequiredArgsConstructor
 public class indexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userNames", user.getName());
+        }
+
         return "index";
     }
 
@@ -32,5 +40,4 @@ public class indexController {
         model.addAttribute("post", dto);
         return "posts-update";
     }
-
 }
